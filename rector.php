734c986cf955ba06d4517b1ca336c5d152b91fc0
mr.incognito <sandeeppant024@gmail.com>
2025-07-13 11:49:15 +0545

    :art: added rector for better code php refactoring and to apply quality rules for code and many more

---
+++ rector.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Set\ValueObject\LevelSetList;
+use RectorLaravel\Set\LaravelSetList;
+
+return static function (RectorConfig $config): void {
+    $config->paths([
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ]);
+
+    $config->import(LaravelSetList::LARAVEL_100);
+
+    $config->import(LevelSetList::UP_TO_PHP_84);
+
+    $config->parallel();
+};
+
+
